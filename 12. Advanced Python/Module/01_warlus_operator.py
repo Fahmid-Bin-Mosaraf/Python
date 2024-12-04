34e46c08cc28f37b515ae09282d59ca3a46aaf81
Fahmid-Bin-Mosaraf <fahmidfamel396@gmail.com>
2024-12-04 11:33:04 +0600

    advanced python

---
+++ 12. Advanced Python/Module/01_warlus_operator.py
@@ -0,0 +1,7 @@
+num = [1, 2, 3, 4, 5]
+
+while(n := len(num)) > 0:
+    print(num.pop())
+
+
+# he walrus operator (:=) assigns a value to n while also allowing its value to be checked in the condition.
